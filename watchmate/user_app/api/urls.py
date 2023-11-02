b5bdf839db33e8c2494832c983aa3a9a58561354
kongnyuy livingston <kongnyuylivingston2@gmail.com>
2023-11-02 13:22:19 +0100

    added an endpoint call logout that will allow me to logout and delete a token

--- watchmate/user_app/api/urls.py
+++ watchmate/user_app/api/urls.py
@@ -4,6 +4,6 @@ from user_app.api.views import *
 
 urlpatterns = [
     path('login/', obtain_auth_token, name = 'login'),           #allow use to login for generating new token.add()
-    path('register/', registration_view, name = 'register')
-
+    path('register/', registration_view, name = 'register'),
+    path('logout_view/',  logout_view, name = 'logout_view'),
 ]
