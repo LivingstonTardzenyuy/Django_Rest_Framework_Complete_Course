faa7bec141bea2abff872d12d99c5b64e6eb2388
kongnyuy livingston <kongnyuylivingston2@gmail.com>
2023-11-14 10:30:17 +0100

    added 3 test for signup, signin and logout

--- watchmate/user_app/tests.py
+++ watchmate/user_app/tests.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
-from django.urls import reverse
+from django.urls import reverse                 #allows me to direct to a particular endpoint.
 from rest_framework import status
 
 from rest_framework.authtoken.models import Token 
@@ -15,7 +15,7 @@ class RegisterTestCase(APITestCase):
             "password2": "NewPassword@123"
         }
 
-        response = self.client.post(reverse('register'), data)
+        response = self.client.post(reverse('register'), data)      
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)     #matching our response with our status code to see if they are the same.
         # self
 
@@ -33,4 +33,14 @@ class LoginLogOutTestCase(APITestCase):
         }
 
         response = self.client.post(reverse('login'), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+
+    def test_logout(self):
+        self.token = Token.objects.get(
+            user__username = "example")
+        
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(reverse('logout_view'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
